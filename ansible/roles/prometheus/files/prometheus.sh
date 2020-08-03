3300fae43b794fe35dcf0d297717fd0cbdbc3201
cathal <cathalcorbett3@gmail.com>
2020-08-03 09:14:32 +0100

    New brach setup with ansible directory

---
+++ ansible/roles/prometheus/files/prometheus.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Download prometheus and copy utilities to where they should be in the filesystem
+#VERSION=2.2.1
+VERSION=$(curl https://raw.githubusercontent.com/prometheus/prometheus/master/VERSION)
+wget https://github.com/prometheus/prometheus/releases/download/v${VERSION}/prometheus-${VERSION}.linux-amd64.tar.gz
+tar xvzf prometheus-${VERSION}.linux-amd64.tar.gz
+
+sudo cp prometheus-${VERSION}.linux-amd64/prometheus /usr/local/bin/
+sudo cp prometheus-${VERSION}.linux-amd64/promtool /usr/local/bin/
+sudo cp -r prometheus-${VERSION}.linux-amd64/consoles /etc/prometheus
+sudo cp -r prometheus-${VERSION}.linux-amd64/console_libraries /etc/prometheus
