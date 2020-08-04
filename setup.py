a90660d2bd99d79ef1de79ba8500b653c5846d4a
cathal <cathalcorbett3@gmail.com>
2020-08-04 09:04:27 +0100

    New pypi version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='job_queue_exporter',
-    version='0.3',
+    version='0.4',
     description='Prometheus Exporter for job information associated with HPC Scheduler',
     url='http://gitlab.com/surfprace/cathal',
     author='Cathal Corbett',
