f5d35ecf5c65b58744c3922604c182b7fad1f2d0
cathal <cathalcorbett3@gmail.com>
2020-08-11 13:57:19 +0100

    update prometheus.yml target path

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='job_queue_exporter',
-    version='0.7',
+    version='0.8',
     description='Prometheus Exporter for job information associated with HPC Scheduler',
     url='http://gitlab.com/surfprace/cathal',
     author='Cathal Corbett',
